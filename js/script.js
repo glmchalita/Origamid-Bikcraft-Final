2911c3b8612076815b9b38473dd359014921418d
Guilherme Chalita <guilhermechalita@hotmail.com>
2023-04-12 19:58:12 -0300

    Add hamburguer menu no FAQ

--- js/script.js
+++ js/script.js
@@ -21,3 +21,20 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  pergunta.setAttribute("aria-expanded", resposta.classList.contains("ativa"));
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
